007ff876cc4a50e81854dac3563769671c618d8d
DanielOlivas <juandanielolivasmartinez@gmail.com>
2024-11-06 22:23:09 -0600

    Update/add: Se agrego interfaz al boton de editar perfil, se modifico el disenyo de la interfaz de los post del propio perfil, se realizaron modificaciones varias al perfil y a los formularios de registro e inicio de sesion

--- ChappFinal/Vistas/Perfil.xaml.cs
+++ ChappFinal/Vistas/Perfil.xaml.cs
@@ -23,4 +23,18 @@ public partial class Perfil : ContentPage
         fotoImage.Source = ImageSource.FromStream(() => foto.OpenReadAsync().Result);
     }
 
+    private void EditProfile_Clicked(object sender, EventArgs e)
+    {
+        EditarPerfilFrame.IsVisible = true;
+    }
+    private void OnCerrarCrearEditClicked(object sender, EventArgs e)
+    {
+        EditarPerfilFrame.IsVisible = false;
+    }
+
+    private async void SeleccionarImageButtonEdit_Clicked(object sender, EventArgs e)
+    {
+        var foto = await MediaPicker.PickPhotoAsync();
+        fotoImageEdit.Source = ImageSource.FromStream(() => foto.OpenReadAsync().Result);
+    }
 }
